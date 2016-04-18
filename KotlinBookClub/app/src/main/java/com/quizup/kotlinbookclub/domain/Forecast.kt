428892f00b23b0f4e444b40d414e7e01049427f8
Alexey Verein <alexey@plainvanillagames.com>
2016-04-18 22:56:22 +0000

    Override get operator in adapter and forecast list, add size to forecast list

--- KotlinBookClub/app/src/main/java/com/quizup/kotlinbookclub/domain/Forecast.kt
+++ KotlinBookClub/app/src/main/java/com/quizup/kotlinbookclub/domain/Forecast.kt
@@ -6,6 +6,10 @@ package com.quizup.kotlinbookclub.domain
  * 02/04/16.
  */
 
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+    operator fun get(position: Int) : Forecast = dailyForecast[position]
+
+    fun size(): Int = dailyForecast.size
+}
 
 data class Forecast(val date: String, val description: String, val low: Double, val high: Double)
